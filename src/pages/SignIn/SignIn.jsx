86a8796e969336dc340ad6b340e959eb9599b50a
armadne <madane.aroul@hotmail.com>
2025-12-14 16:51:36 +0100

    modif fichier SignIn.jsx en backend pour avoir la possibilité de se connecter en tant que admin

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -4,13 +4,13 @@ import * as PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { API_ROUTES, APP_ROUTES } from '../../utils/constants';
 import { useUser } from '../../lib/customHooks';
-import { storeInLocalStorage } from '../../lib/common';
 import { ReactComponent as Logo } from '../../images/Logo.svg';
 import styles from './SignIn.module.css';
 
 function SignIn({ setUser }) {
   const navigate = useNavigate();
   const { user, authenticated } = useUser();
+
   if (user || authenticated) {
     navigate(APP_ROUTES.DASHBOARD);
   }
@@ -19,29 +19,28 @@ function SignIn({ setUser }) {
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [notification, setNotification] = useState({ error: false, message: '' });
+
   const signIn = async () => {
     try {
       setIsLoading(true);
-      const response = await axios({
-        method: 'post',
-        url: API_ROUTES.SIGN_IN,
-        data: {
-          email,
-          password,
-        },
-      });
+      const response = await axios.post(API_ROUTES.SIGN_IN, { email, password });
+
       if (!response?.data?.token) {
         setNotification({ error: true, message: 'Une erreur est survenue' });
+        // eslint-disable-next-line no-console
         console.log('Something went wrong during signing in: ', response);
-      } else {
-        storeInLocalStorage(response.data.token, response.data.userId);
-        setUser(response.data);
-        navigate('/');
+        return;
       }
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('userId', response.data.userId);
+      localStorage.setItem('role', response.data.role);
+
+      setUser(response.data);
+      navigate('/');
     } catch (err) {
-      console.log(err);
-      setNotification({ error: true, message: err.message });
+      // eslint-disable-next-line no-console
       console.log('Some error occured during signing in: ', err);
+      setNotification({ error: true, message: err.message });
     } finally {
       setIsLoading(false);
     }
@@ -50,27 +49,26 @@ function SignIn({ setUser }) {
   const signUp = async () => {
     try {
       setIsLoading(true);
-      const response = await axios({
-        method: 'POST',
-        url: API_ROUTES.SIGN_UP,
-        data: {
-          email,
-          password,
-        },
-      });
+      const response = await axios.post(API_ROUTES.SIGN_UP, { email, password });
+
       if (!response?.data) {
+        // eslint-disable-next-line no-console
         console.log('Something went wrong during signing up: ', response);
         return;
       }
+
       setNotification({ error: false, message: 'Votre compte a bien été créé, vous pouvez vous connecter' });
     } catch (err) {
-      setNotification({ error: true, message: err.message });
+      // eslint-disable-next-line no-console
       console.log('Some error occured during signing up: ', err);
+      setNotification({ error: true, message: err.message });
     } finally {
       setIsLoading(false);
     }
   };
+
   const errorClass = notification.error ? styles.Error : null;
+
   return (
     <div className={`${styles.SignIn} container`}>
       <Logo />
@@ -78,61 +76,45 @@ function SignIn({ setUser }) {
         {notification.message.length > 0 && <p>{notification.message}</p>}
       </div>
       <div className={styles.Form}>
-        <label htmlFor={email}>
+        <label htmlFor="email">
           <p>Adresse email</p>
           <input
-            className=""
             type="text"
             name="email"
             id="email"
             value={email}
-            onChange={(e) => { setEmail(e.target.value); }}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label htmlFor="password">
           <p>Mot de passe</p>
           <input
-            className="border-2 outline-none p-2 rounded-md"
             type="password"
             name="password"
             id="password"
             value={password}
-            onChange={(e) => { setPassword(e.target.value); }}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <div className={styles.Submit}>
           <button
             type="submit"
-            className="
-            flex justify-center
-            p-2 rounded-md w-1/2 self-center
-            bg-gray-800  text-white hover:bg-gray-800"
+            className="flex justify-center p-2 rounded-md w-1/2 self-center bg-gray-800 text-white hover:bg-gray-800"
             onClick={signIn}
           >
-            {isLoading ? <div className="" /> : null}
-            <span>
-              Se connecter
-            </span>
+            {isLoading && <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" />}
+            <span>Se connecter</span>
           </button>
           <span>OU</span>
           <button
             type="submit"
-            className="
-            flex justify-center
-            p-2 rounded-md w-1/2 self-center
-            bg-gray-800  text-white hover:bg-gray-800"
+            className="flex justify-center p-2 rounded-md w-1/2 self-center bg-gray-800 text-white hover:bg-gray-800"
             onClick={signUp}
           >
-            {
-                isLoading
-                  ? <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" /> : null
-              }
-            <span>
-              {'S\'inscrire'}
-            </span>
+            {isLoading && <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" />}
+            <span><span>S&apos;inscrire</span></span>
           </button>
         </div>
-
       </div>
     </div>
   );
@@ -141,4 +123,5 @@ function SignIn({ setUser }) {
 SignIn.propTypes = {
   setUser: PropTypes.func.isRequired,
 };
+
 export default SignIn;
